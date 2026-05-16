cf24f3abd96cf8805deb90c88177abdd10401bb8
gastongalumar <gaston.galumar@hotmail.com>
2026-05-16 16:55:58 -0300

    CONFIGURACION INICIAL Y PACKAGES.

---
+++ src/main/java/GESTOR_CINEMAR_CENTER/DEV/CineBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package GESTOR_CINEMAR_CENTER.DEV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GESTOR_CINEMAR_CENTER/DEV/CineBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package GESTOR_CINEMAR_CENTER.DEV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
